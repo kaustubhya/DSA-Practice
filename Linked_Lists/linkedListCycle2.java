9b1be7a13a98dd7b6222aa988fa94a2172da526c
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2023-10-06 07:40:15 +0530

    here

--- Linked_Lists/linkedListCycle2.java
+++ Linked_Lists/linkedListCycle2.java
@@ -1,21 +1,21 @@
-package LinkedList;
+package Linked_Lists;
 
     /**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+ * Definition for singly-linked list. */
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+ 
 
  // Floyd's Cycle Detection Algorithm 
 
 
-public class LinkedListCycle2 {
+public class linkedListCycle2 {
 
 
     public ListNode detectCycle(ListNode head) {
