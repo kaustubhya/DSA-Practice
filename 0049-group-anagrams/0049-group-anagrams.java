ef6df522b839fcb3800aa73a21b63af92f39fe87
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2023-10-12 01:51:41 +0530

    Time: 6 ms (73.47%), Space: 46.5 MB (89.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        
+        // I will use hashmaps here for grouping
+        // see explaination in notes.md file
+        Map<String, List<String>> myMap = new HashMap<>();
+        // here Map<key, value>
+        
+        
+        int n = strs.length;
+        
+        for(int i = 0; i < n; i++) {
+            // 1. imp line, convert string to char array
+            char[] temp = strs[i].toCharArray();
+            // 2. sort it
+            Arrays.sort(temp);
+            
+            // 3. convert back char array to string
+            String mystr = new String(temp);
+            
+            
+            // now after sorting, we get a key, if a string matches that key (order of chars not necessary), we put that in a key value pair array and keep putting similar strings in that same array which matches that key value pair array, 
+            
+            // if we get a new key make a different key value pair array and do the same 
+            
+            
+            // 4. if 0 keys,  PUT KEY-VALUE
+            if(myMap.containsKey(mystr) == false) {
+                // it means the map 'myMap' does not have a key currently i.e. it is empty
+                
+                // putting key inside it now with a new arrayList for values, key is of string type
+                myMap.put(mystr, new ArrayList<>());
+            }
+            
+            // now fetch values from the 'strs' array as we are in a 'for' loop and match it with the corresponding keys
+            
+            // 5. GET key and ADD values
+            
+            myMap.get(mystr).add(strs[i]);
+            } // for loop ends
+            
+        
+        // now we have all key value pairs
+        
+        // add all values (which are array lists) in the answer list (which is list of lists)
+        
+        
+        // 6. ADD ALL values
+        ans.addAll(myMap.values());
+                
+        
+        
+        return ans;
+    }
+}
