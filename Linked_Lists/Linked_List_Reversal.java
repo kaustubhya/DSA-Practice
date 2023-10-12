2245d56339a2be092f15f400a58d1c8a2f327ed3
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2023-10-12 13:38:51 +0530

    here

---
+++ Linked_Lists/Linked_List_Reversal.java
@@ -0,0 +1,44 @@
+package Linked_Lists;
+
+public class Linked_List_Reversal {
+    // Reversing the Linked List 'In Place' (Iterative Method)
+
+//   Definition for singly-linked list.
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+ 
+
+    public ListNode reverseList(ListNode head) {
+    // using prev, current and next pointers
+    // given only head, no tail
+        
+        // edge case
+        if(head == null) {
+            return head;
+            // manage the next node as here current = null = head so next will give error as next will be after null
+        }
+
+    ListNode prev = null;
+    ListNode current = head;
+    ListNode next = current.next;
+
+    while(current != null) {
+        current.next = prev;
+        prev = current;
+        current = next;
+
+        if(next != null) {
+            next = next.next;
+        }
+    }
+
+    return prev;
+
+    }
+
+}
