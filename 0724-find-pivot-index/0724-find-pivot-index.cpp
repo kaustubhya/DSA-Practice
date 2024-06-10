b8b6667a07d9dc466ce9de8ced39a35222dc95e9
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-06-10 19:41:47 +0530

    Time: 765 ms (8.50%), Space: 34 MB (34.39%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,15 +1,38 @@
 class Solution {
+
+public:
+    int BruteForce(vector<int>& nums) {
+        int n = nums.size();
+        
+
+        for(int i = 0; i < n; i++) {
+            // \U0001f6d1\U0001f6d1\U0001f6d1 Reset the sums after every iteration of i
+            int leftSum = 0;
+            int rightSum = 0;
+
+
+            for(int j = 0; j < i; j++) {
+                leftSum += nums[j];
+            }
+
+            for(int k = i + 1; k < n; k++) {
+                rightSum += nums[k];
+            }
+
+            if(leftSum == rightSum) {
+                return i;
+                // the pivot index
+            }        
+        }
+
+        return -1;
+        // no pivot index found
+    }
+
 public:
     int pivotIndex(vector<int>& nums) {
-        int leftSum = 0;
-        int totalSum = accumulate(nums.begin(),nums.end(),0);
-        int i = 0;
-        while(i < nums.size()) {
-            if(leftSum == totalSum - leftSum - nums[i]) {
-                return i;
-            }
-            leftSum += nums[i++];
-        }
-        return -1;
+        int ans = BruteForce(nums);
+        return ans;
+
     }
 };
